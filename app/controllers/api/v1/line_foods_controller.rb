385481d2619cf4558bcfa706bdb682e95fa59ab5
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-11-08 13:33:54 -0800

    仮注文の作成 create

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -2,7 +2,7 @@
 module Api
   module V1
     class LineFoodsController < ApplicationController
-      before_action :set_food, only: %i[create]
+      before_action :set_food, only: %i[create] #before_actionではそのコントローラーのメインアクションに入る"前"に行う処理を挟める -> callback
 
       def create
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
@@ -12,14 +12,14 @@ module Api
           }, status: :not_acceptable
         end
 
-        set_line_food(@ordered_food)
+        set_line_food(@ordered_food) # line_foodインスタンス生成
 
-        if @line_food.save
+        if @line_food.save # DBに保存
           render json: {
             line_food: @line_food
-          }, status: :created
+          }, status: :created # saveが成功した場合 -> status: :createdと保存したデータを返す
         else
-          render json: {}, status: :internal_server_error
+          render json: {}, status: :internal_server_error # if @line_food.saveでfalseだった場合 -> render json~がreturn, もしフロントエンドでエラーの内容に応じて表示を変えるような場合にここでHTTPレスポンスステータスコードが500系になる
         end
       end
 
@@ -30,14 +30,14 @@ module Api
       end
 
       def set_line_food(ordered_food)
-        if ordered_food.line_food.present?
+        if ordered_food.line_food.present? # 新しくline_foodを生成するのか、既に同じfoodに関するline_foodが存在する場合を判断している
           @line_food = ordered_food.line_food
           @line_food.attributes = {
             count: ordered_food.line_food.count + params[:count],
             active: true
-          }
+          } #present?がtrueの場合 -> 既存のline_foodインスタンスの既存の情報を更新(count, activeの２つをupdate)
         else
-          @line_food = ordered_food.build_line_food(
+          @line_food = ordered_food.build_line_food( # 新しくインスタンスを作成
             count: params[:count],
             restaurant: ordered_food.restaurant,
             active: true
@@ -64,3 +64,5 @@ end
 # LineFood.active.other_restaurant(@ordered_food.restaurant.id)は複数のscope(active, other_resrtaurant)を組み合わせて「他店舗でアクティブなLineFood」をActiveRecord_Relationのかたちで取得　-> それが存在するかどうかをexists?で判断　-> if it's true, then return JSONデータ
 
 # JSONの中身にはexisting_restaurantで既に作成されている他店舗の情報と、new_restaurantでこのリクエストで作成しようとした新店舗の情報の２つをreturn, and return "406 Not Acceptable"HTTPレスポンスステータスコード
+
+# 「早期リターン」とは複雑あるいは重いメイン処理を実行する前に、この処理が不要なケースに当てはまるかどうかをifなどでチェックし、その場合にメイン処理に入らせないことを指す
