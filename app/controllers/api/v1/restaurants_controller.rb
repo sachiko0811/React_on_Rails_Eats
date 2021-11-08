3a33399e74552b208293b0c15f04025f5abf046a
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-11-08 00:17:27 -0800

    restaurants_contoroller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,15 +1,21 @@
 # --- ここから追加 ---
-module Api
+module Api #名前空間(module -> 部品), moduleでclassをまとめたり、名前空間を表すことができる
   module V1
-    class RestaurantsController < ApplicationController
-      def index
-        restaurants = Restaurant.all
+    class RestaurantsController < ApplicationController #rubyでは継承を継承先 < 継承元で表す　=> ApplicationConrollerを継承したRestaurantsController
+      def index # index method
+        restaurants = Restaurant.all # Restaurantsモデルを全取得してrestaurantsという変数に代入
 
-        render json: {
+        render json: {  # JSON形式でデータを返却
           restaurants: restaurants
-        }, status: :ok
+        }, status: :ok # -> リクエストが成功、(HTTP) 200 OKとともにデータを返す
       end
     end
   end
 end
 # --- ここまで追加 ---
+
+# 名前空間を設定すると、一つのモデルの中で、いくつかのCRUD処理を行える、機能ごとに実装追加可能 -> codeが複雑化しない
+
+#　このコントローラーではリクエストにパラメーターは不要で、Restaurant.allがreturn
+
+#class A < B => Bを継承したAを定義できる
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_044355) do
+ActiveRecord::Schema.define(version: 2021_11_04_093616) do
 
   create_table "foods", force: :cascade do |t|
     t.bigint "restaurant_id", null: false
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 2021_11_01_044355) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["restaurant_id"], name: "index_foods_on_restaurant_id"
+  end
+
+  create_table "line_foods", force: :cascade do |t|
+    t.bigint "food_id", null: false
+    t.bigint "restaurant_id", null: false
+    t.bigint "order_id"
+    t.integer "count", default: 0, null: false
+    t.boolean "active", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_line_foods_on_food_id"
+    t.index ["order_id"], name: "index_line_foods_on_order_id"
+    t.index ["restaurant_id"], name: "index_line_foods_on_restaurant_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -37,4 +50,7 @@ ActiveRecord::Schema.define(version: 2021_11_01_044355) do
   end
 
   add_foreign_key "foods", "restaurants"
+  add_foreign_key "line_foods", "foods"
+  add_foreign_key "line_foods", "orders"
+  add_foreign_key "line_foods", "restaurants"
 end
