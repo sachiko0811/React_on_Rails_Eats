92aa931d2c65e0e1b6caea65b1bc12e205f56756
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-11-11 15:34:05 -0800

    frontend side

---
+++ frontend/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+// components
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        {/* // 店舗一覧ページ */}
+        <Route
+          exacts
+          path="/restaurants" //path="設定したいPATH"
+        >
+          <Restaurants />
+        </Route>
+        {/* // フード一覧ページ */}
+        <Route
+          exact
+          path="/foods"
+        >
+          <Foods />
+        </Route>
+        {/* // 注文ページ */}
+        <Route
+          exact
+          path="/orders"
+        >
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
+
+// React Router -> <Router>で全体を囲む, <Switch>でルーティング先のコンポーネントを囲む
+// ex => ページ共通のheaderやfooterがある場合には<Switch>の外に出しておく
+// <Route>で実際に１ページへのルーティングを表す ... <Route>にpropsを渡すことでリクエストに対応して囲ったコンポーネントを描画 
+// exact -> propsではデフォルトではfalseで、trueにするとexact={true}としなくても、PATHの完全一致の場合にのみコンポーネントをレンダリングする
+
+// props: 親・子コンポーネント間でのデータの「受け渡し口」
+// -> 親コンポーネントから渡したいデータをpropsという箱に入れ、子コンポーネントもpropsという箱を受け取れるようにし、子コンポーネントの中でpropsの箱の中のデータを参照
+
+// export const ChildComponent = (props) => {
+//   // props.idの方法で取得する方法
+//   const hoge = () => { props.id }
+// }
+
+// export const ChildComponent = ({ id }) => {
+//   // idと直接key名を指定してデータを参照する方法
+//   const hoge = () => { id }
+// }
